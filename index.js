fc38fc3f24b8ed511fa99ae4724bdab54e86a88f
Faiz Halde <fhalde@paypalcorp.com>
2017-01-10 18:16:37 +0530

    to rxjs migration

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 'use strict'
 
-function q(name) {
-    if (name[0] == '#') {
-        return document.getElementById(name.substring(1));
-    }
-    if (name[0] == '.') {
-        return document.getElementsByClassName(name.substring(1));
-    }
+function currentScrollTop() {
+    return (window.pageYOffset || document.documentElement.scrollTop) 
+            - 
+           (document.documentElement.clientTop || 0);
 }
 
 function mk(tag) {
@@ -34,14 +31,32 @@ function mk(tag) {
 }
 
 var events = [
-    { content: 'Born', date: 'March 1994' },
-    { content: 'High school graduation', date: 'March 2011' },
-    { content: 'Internship at IBM', date: 'June 2014' },
-    { content: 'College graduation', date: 'July 2015' },
-    { content: 'Joined PayPal Inc', date: 'July 2015' }
+    {
+        content: 'Born',
+        date: 'March 1994'
+    },
+    {
+        content: 'High school graduation',
+        date: 'March 2011'
+    },
+    {
+        content: 'Internship at IBM',
+        date: 'June 2014'
+    },
+    {
+        content: 'College graduation',
+        date: 'July 2015'
+    },
+    {
+        content: 'Joined PayPal Inc',
+        date: 'July 2015'
+    }
 ];
-var timedot = q('.timedot')[0];
-var ev_name = q('.event-name')[0];
+var colors = ['#2196f3', '#fafafa'];
+var titles = ['', 'ABOUT ME', 'SKILLS', 'TIMELINE', 'WEB PRESENCE'];
+
+var timedot = document.getElementsByClassName('timedot')[0];
+var ev_name = document.getElementsByClassName('event-name')[0];
 
 ! function createTimeline() {
     if (events.length == 0) {
@@ -54,27 +69,22 @@ var ev_name = q('.event-name')[0];
     mk('div').class('circles').onmouseover(i).append(timedot);
 }();
 
-
-var colors = ['#2196f3', '#fafafa'];
-var titles = ['', 'ABOUT ME', 'SKILLS', 'TIMELINE', 'WEB PRESENCE'];
-var divs = q('.section');
-
-! function changeHeaderOnScroll() {
-    var header = q('.headers')[0];
-    document.addEventListener('scroll', function(e) {
-        var doc = document.documentElement;
-        var scrollTop = window.pageYOffset || doc.scrollTop;
-        var top = scrollTop - (doc.clientTop || 0);
-        for (var elem in divs) {
-            var node = divs[elem];
-            if (top < (node.offsetTop + node.clientHeight)) {
-                header.innerHTML = titles[elem];
-                header.style.color = colors[parseInt(elem) % 2];
-                var factor = 1 - ((scrollTop - node.offsetTop) / node.clientHeight);
-                header.style.opacity = factor;
-                header.style.transform = 'scale(' + factor + ')';
-                break;
+var scroll$ = Rx.Observable.fromEvent(document, 'scroll').map(currentScrollTop).startWith(currentScrollTop());
+var titles$ = Rx.Observable.merge(
+    ...[].map.call(q('.section'), (div, ind) => {
+        return scroll$.filter((top) => {
+            return div.offsetTop <= top && (top < (div.offsetTop + div.clientHeight))
+        }).map(() => {
+            return {
+                title: titles[ind],
+                color: colors[ind % 2],
+                opacity: 1 - (currentScrollTop() - div.offsetTop) / div.clientHeight
             }
-        }
-    });
-}();
+        })
+    })).subscribe((title) => {
+    var header = document.getElementsByClassName('headers')[0];
+    header.innerHTML = title.title;
+    header.style.color = title.color;
+    header.style.opacity = title.opacity;
+    header.style.transform = 'scale(' + title.opacity + ')';
+});
